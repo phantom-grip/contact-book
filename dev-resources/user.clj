dff6c96252c71beadd416169a231580fb8a9ecbd
Yanis <yanis.urbis@gmail.com>
2019-05-10 16:53:29 +0300

    Add working resolver

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -1,12 +1,33 @@
 (ns user
   (:require
     [contact_book_backend.schema :as s]
-    [com.walmartlabs.lacinia :as lacinia]))
+    [com.walmartlabs.lacinia :as lacinia]
+    [clojure.walk :as walk])
+  (:import (clojure.lang IPersistentMap)))
 
 (def schema (s/load-schema))
 
+(defn simplify
+  "Converts all ordered maps nested within the map into standard hash maps, and
+   sequences into vectors, which makes for easier constants in the tests, and eliminates ordering problems."
+  [m]
+  (walk/postwalk
+    (fn [node]
+      (cond
+        (instance? IPersistentMap node)
+        (into {} node)
+
+        (seq? node)
+        (vec node)
+
+        :else
+        node))
+    m))
+
 (defn q
   [query-string]
-  (lacinia/execute schema query-string nil nil))
+  (-> (lacinia/execute schema query-string nil nil)
+      simplify))
 
-(comment (q "{ contact_by_id(id: \"foo\") { id name company_name }}"))
+(comment (q "{ contact_by_id(id: \"foo\") { id name company_name }}")
+         (q "{ contact_by_id(id: \"1\") { id name company_name }}"))
--- src/contact_book_backend/schema.clj
+++ src/contact_book_backend/schema.clj
@@ -6,10 +6,20 @@
     [com.walmartlabs.lacinia.schema :as schema]
     [clojure.edn :as edn]))
 
+(defn resolve-contact-by-id
+  [contacts-map context args value]
+  (let [{:keys [id]} args]
+    (get contacts-map id)))
+
 (defn resolver-map
   []
-  {:query/contact_by_id (fn [context args value]
-                          nil)})
+  (let [cb-data (-> (io/resource "cb-data.edn")
+                    slurp
+                    edn/read-string)
+        contacts-map (->> cb-data
+                          :contacts
+                          (reduce #(assoc %1 (:id %2) %2) {}))]
+    {:query/contact_by_id (partial resolve-contact-by-id contacts-map)}))
 
 (defn load-schema
   []
