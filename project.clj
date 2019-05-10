36be373ce97bb116e2032da39f9c76db04f6a705
Yanis <yanis.urbis@gmail.com>
2019-05-10 15:43:23 +0300

    Add project description & project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject contact-book-backend "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A tiny contact book in Clojure"
+  :url "https://github.com/phantom-grip/contact-book-backend"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
