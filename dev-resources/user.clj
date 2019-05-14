f0a4f5d5b31fda75515d021e52f9d862b677b2bb
Yanis <yanis.urbis@gmail.com>
2019-05-14 22:10:52 +0300

    Add graphiql server

--- dev-resources/user.clj
+++ dev-resources/user.clj
@@ -1,8 +1,11 @@
 (ns user
   (:require
-    [contact_book_backend.schema :as s]
-    [com.walmartlabs.lacinia :as lacinia]
-    [clojure.walk :as walk])
+   [contact_book_backend.schema :as s]
+   [com.walmartlabs.lacinia :as lacinia]
+   [clojure.walk :as walk]
+   [com.walmartlabs.lacinia.pedestal :as lp]
+   [io.pedestal.http :as http]
+   [clojure.java.browse :refer [browse-url]])
   (:import (clojure.lang IPersistentMap)))
 
 (def schema (s/load-schema))
@@ -12,17 +15,17 @@
    sequences into vectors, which makes for easier constants in the tests, and eliminates ordering problems."
   [m]
   (walk/postwalk
-    (fn [node]
-      (cond
-        (instance? IPersistentMap node)
-        (into {} node)
+   (fn [node]
+     (cond
+       (instance? IPersistentMap node)
+       (into {} node)
 
-        (seq? node)
-        (vec node)
+       (seq? node)
+       (vec node)
 
-        :else
-        node))
-    m))
+       :else
+       node))
+   m))
 
 (defn q
   [query-string]
@@ -31,3 +34,30 @@
 
 (comment (q "{ contact_by_id(id: \"foo\") { id name company_name }}")
          (q "{ contact_by_id(id: \"1\") { id name company_name phones { phone contact { id name } }}}"))
+
+(defonce server nil)
+
+(defn start-server
+  [_]
+  (let [server (-> schema
+                   (lp/service-map {:graphiql true})
+                   http/create-server
+                   http/start)]
+    (browse-url "http://localhost:8888/")
+    server))
+
+(defn stop-server
+  [server]
+  (http/stop server)
+  nil)
+
+(defn start
+  []
+  (alter-var-root #'server start-server)
+  :started)
+
+(defn stop
+  []
+  (alter-var-root #'server stop-server)
+  :stopped)
+
--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.walmartlabs/lacinia "0.33.0-alpha-3"]]
+                 [com.walmartlabs/lacinia-pedestal "0.11.0"]
+                 [io.aviso/logging "0.3.2"]]
   :repl-options {:init-ns contact-book-backend.core})
